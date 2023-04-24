85c14373726894cbddea182c2924d2c191929963
Folashade Bassey <adegbembofolashade@gmail.com>
2023-04-24 13:32:44 -0700

    print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main-prints number zero to nine
+ * Return: returns the value zero
+ */
+int main(void)
+{
+
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+	printf("%d", n);
+	}
+	printf("\n");
+	return (0);
+}
