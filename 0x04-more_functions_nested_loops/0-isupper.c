c2e3c92dc74c599bb2bfba647d1a3a2cc506f3d8
Folashade Bassey <adegbembofolashade@gmail.com>
2023-04-28 11:42:08 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * main - check the code.
+ * @c: takes an integer value
+ * Return: Always 0.
+ */
+
+int _isupper(int c);
+
+{
+		int i;
+
+		if (i >= 65 && i <= 90)
+		{
+			return (1);
+		}
+		else
+		{
+			return (0);
+		}
+
+}
