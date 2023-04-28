cf4c2577dd6863b81a044c13488c7ebff4ddedff
Folashade Bassey <adegbembofolashade@gmail.com>
2023-04-28 12:54:35 -0700

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/* betty style doc for function main goes there */
+/**
+ * _isdigit- is the prototype
+ * @c: takes an integer value
+ * Return: this returns 1 or 0 if otherwise
+ */
+
+int _isdigit(int c)
+
+{
+	int c;
+
+	for (c = 48; c <= 57; c++)
+	{
+		return (1);
+	}
+	return (0);
+}
