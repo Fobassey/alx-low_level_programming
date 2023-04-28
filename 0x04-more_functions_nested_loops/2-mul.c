dee754e45273c927b95a7ef8c8b60d223b0d6f93
Folashade Bassey <adegbembofolashade@gmail.com>
2023-04-28 13:25:43 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/* betty style doc for function main goes there */
+/**
+ * mul-this multiplies two integers
+ * @a: this are the integers
+ * @b: this are the integers
+ * Return: returns the equivalent values
+ */
+
+int mul(int a, int b)
+
+{
+	return (a * b);
+}
