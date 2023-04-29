50418a0d8b03518d3ae6fc6d6703d0b0c40a0386
Folashade Bassey <adegbembofolashade@gmail.com>
2023-04-28 23:32:26 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,15 +2,14 @@
 /* betty style doc for function main goes there */
 /**
  * print_most_numbers- this does not print 2 and 4
- * @i: takes the numbers
  * Return: returns the equivalent values
  */
-void print_most_numbers(void);
+void print_most_numbers(void)
 
 {
-	char i;
+char i;
 
-	for (i = '0'; i >= '9'; i++)
+	for (i = '0'; i <= '9'; i++)
 	{
 		if (!(i == '2' || i == '4'))
 		_putchar(i);
