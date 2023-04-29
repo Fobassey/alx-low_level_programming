cadd52ddd37afe7aa9700157342673e4794f0c9e
Folashade Bassey <adegbembofolashade@gmail.com>
2023-04-29 09:42:58 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,10 +1,11 @@
-#include <main.h>
+#include "main.h"
 /* betty style doc for function main goes there */
 /**
- * main-prints number zero to nine
+ * _isalpha- prints number zero to nine
+ * @c: takes an integer
  * Return: returns the value zero
  */
-int _isalpha(int c);
+int _isalpha(int c)
 
 {
 	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
@@ -15,5 +16,5 @@ int _isalpha(int c);
 	{
 		return (0);
 	}
-	_putchar('\n';
+	_putchar('\n');
 }
