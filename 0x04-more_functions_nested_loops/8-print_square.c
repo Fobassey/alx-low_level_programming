41ee5b956ea62e58a8e93eb24725dd55c18b0f1d
Folashade Bassey <adegbembofolashade@gmail.com>
2023-04-28 22:09:41 -0700

    8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/* betty style doc for function main goes there */
+/**
+ * print_square-this prints a square
+ * @size: returns an integer
+ * @i: returns an integer
+ * @j: returns an integer
+ * Return: returns the equivalent values
+ */
+
+void print_square(int size)
+
+{
+	int i, j;
+
+	if (size <= 0)
+	_putchar ('\n');
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
