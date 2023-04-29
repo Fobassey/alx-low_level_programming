b33575eea5030d10ad5ea3aaae674fbc77b1dc09
Folashade Bassey <adegbembofolashade@gmail.com>
2023-04-28 21:56:17 -0700

    7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/* betty style doc for function main goes there */
+/**
+ * print_diagonal-thisprints a diagonal line
+ * @n: takes an integer
+ * @i: takes an integer
+ * @j: takes an integer
+ * Return: returns the equivalent values
+ */
+
+void print_diagonal(int n)
+
+{
+	int i, j;
+
+	if (n <= 0)
+	_putchar ('\n');
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < i; j++)
+		{
+			_putchar(' ')
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
+}
