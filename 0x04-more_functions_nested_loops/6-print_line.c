2a4246effc3ac5e1e4f1e677a20ca0e603fcb4d4
Folashade Bassey <adegbembofolashade@gmail.com>
2023-04-28 21:32:37 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/* betty style doc for function main goes there */
+/**
+ * print_line- this prints a line
+ * @n: takes an integer
+ * @i: takes an integer
+ * Return: returns the equivalent values
+ */
+
+void print_line(int n)
+
+{
+	int i;
+
+	for (i = 0; i <= n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
