21eda77588f5746e6b0ab8bec34b2c43e339016d
Folashade Bassey <adegbembofolashade@gmail.com>
2023-05-01 04:16:58 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,27 +1,27 @@
-#include <main.h>
+#include "main.h"
 /* betty style doc for function main goes there */
 /**
- * main-prints number zero to nine
- * @n: the input number
+ * print_sign- prints number zero to nine
+ * @n: ia an integer
  * Return: returns the value zero
  */
-int print_sign(int n);
+int print_sign(int n)
 
 {
 	if (n > 0)
 	{
-		else if (n == 0)
-		{
-			else
-			{
-			_putchar(45);
-			return (-1);
-			}
+		_putchar(43);
+		return (1);
+	}
+	else if (n == 0)
+	{
 		_putchar(48);
 		return (0);
-		}
-	_putchar(43);
-	return (1);
+	}
+	else
+	{
+		_putchar(45);
+		return (-1);
 	}
 	_putchar('\n');
 }
