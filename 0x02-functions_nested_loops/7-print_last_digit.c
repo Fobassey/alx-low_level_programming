165a7751d552a9dd5bb11f32b6922d40311a2e64
Folashade Bassey <adegbembofolashade@gmail.com>
2023-05-01 05:31:45 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,18 +1,18 @@
-#include <main.h>
+#include "main.h"
 /* betty style doc for function main goes there */
 /**
- * main-prints number zero to nine
+ * print_last_digit- prints number zero to nine
  * @l: takes an integer
  * Return: returns the value zero
  */
-int print_last_digit(int l);
+int print_last_digit(int l)
 
 {
 	int n;
 
 	n = l % 10;
 
-	if (n < 48)
+	if (n < 0)
 	{
 		_putchar(-n + 48);
 		return (-n);
