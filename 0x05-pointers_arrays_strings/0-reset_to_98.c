0f7dacbcae0b125f784a4da96d5f1d065c4194f4
Folashade Bassey <adegbembofolashade@gmail.com>
2023-05-02 20:53:28 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/* betty style doc for function main goes there */
+/**
+ * reset_to_98- takes a pointerr and updates its value
+ * @n: is pointer with a variable whose address is an int type
+ * Return: returns the value zero
+ */
+
+void reset_to_98(int *n)
+
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,30 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* betty style doc for function main goes there */
+/**
+ * main-this includes a list of all prototypes
+ * Return: returns the equivalent values
+ */
+
+void reset_to_98(int *n);
+
+int _strlen(char *s);
+
+void _puts(char *str);
+
+void print_rev(char *s);
+
+void rev_string(char *s);
+
+void puts2(char *str);
+
+void puts_half(char *str);
+
+void print_array(int *a, int n);
+
+char *_strcpy(char *dest, char *src);
+
+int _putchar(char c);
+
+#endif /* MAIN_H */
